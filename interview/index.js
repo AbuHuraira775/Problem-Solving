816674bc347a768ee84bcf902c8e99d52102bc6a
AbuHuraira775 <abuhurairah775@gmail.com>
2024-11-02 21:18:12 +0500

    interview questions

---
+++ interview/index.js
@@ -0,0 +1,38 @@
+// Q:01 What is shadow DOM?
+// Q:02 What is prop drilling?
+// Q:03 What is throttling?
+// Q:04 What is debouncing?
+// Q:05 What is callback?
+// Q:06 What is promise?
+// Q:07 What is async await?
+// Q:08 What is Execution Context?
+// Q:09 What is callback hell?
+// Q:10 What is SQL and NoSQL?
+// Q:11 SQL vs NoSQL?
+// Q:12 var vs let vs const?
+// Q:13 What is hoisting?
+// Q:14 What is arrow function?
+// Q:15 What are the states of the promises?
+// Q:16 What is the pass by value and pass by reference?
+// Q:17 How execution context is created for the var and function?
+// Q:18 Props vs state
+// Q:19 What is HOC? Give example for the ecommerce website
+// Q:20 What is the difference between the Context and Redux? and why use Redux instead of the context in large application?
+// Q:21 Is redux store is immutable or mutable?
+// Q:22 What is virtual DOM? and waht algo React use to compare the current and the previous DOM?
+// Q:23 What is the difference between the map and filter? and give the main difference between them. 
+// If we can achieve the map behaviour from the filter so why we use map?
+// Q:24 is node js is single threaded or multi threaded?
+// Q:25 How node.js performs as a multi threaded?
+// Q:26 How can run five promisses in parallel?
+// Q:27 What does preventDefault() do?
+// Q:28 Tell some array methods?
+// Q:29 What does useRef do?
+// Q:30 does setter update the state (object) when pass by reference?
+// Q:31 What is the structure of mongoDB instead of the table (rows and columns)?
+// Q:32 Write code for fubunacci series?
+// Q:33 Write code for pelledrom?
+// Q:34 Write code to find the missing range in the array?
+// Q:35 Write code to find the same string instead of the order? like 'ali' and 'lia' are same 
+// Q:37 total 9 balls each ball weight is 1kg instead of one ball that is 1.5 kg. 
+// What are the best and worst case to find that ball?
