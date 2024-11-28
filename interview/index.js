139963d149a7429230b45d1fe9b946918bb7ca05
AbuHuraira775 <abuhurairah775@gmail.com>
2024-11-28 11:00:48 +0500

    problem 09

--- interview/index.js
+++ interview/index.js
@@ -1,19 +1,58 @@
 // Q:01 What is shadow DOM?
+
+// encapsulate the internal structure and styling of a component, 
+// ensuring it doesn't interfere with or get affected by the rest of the page. 
+
 // Q:02 What is prop drilling?
+
 // Q:03 What is throttling?
-// Q:04 What is debouncing?
+
+// if we have to execute a function after a certain amount of time, based on the event 
+// but it countineusly call the function when we start the event so we use the throttling
+// like we are scrolling the page and we have to call the function after the 100ms 
+// and it increasing the count till scrolling is stop 
+
+// technique used in programming to control how often a particular function 
+// can be executed over a specified period of time.
+// It ensures that the function is not called too frequently,
+// but instead is called only after a certain amount of time has passed.
+// The function is executed at regular intervals,
+
+
+
+// // Q:04 What is debouncing?
+
+// unlike throttling the debounce technique will ensure that a function is not called until a 
+// certain amount of time has passed without it being called.
+// but it calls the function once after the event is stop
+
 // Q:05 What is callback?
+
 // Q:06 What is promise?
+// it is a JS object that handles async operations.
+// it has three states pending, resolve, reject. 
+
 // Q:07 What is async await?
+// async means that a function will return a promise.
+// await means that we should wait for the promise to resolve before continuing the code execution.
+
 // Q:08 What is Execution Context?
 // Q:09 What is callback hell?
 // Q:10 What is SQL and NoSQL?
+// SQL is a structured query language that is used to manage the relational database system.
+// NoSQL is a non-relational database system that is used to manage the unstructured data.
+
+
 // Q:11 SQL vs NoSQL?
 // Q:12 var vs let vs const?
 // Q:13 What is hoisting?
 // Q:14 What is arrow function?
 // Q:15 What are the states of the promises?
 // Q:16 What is the pass by value and pass by reference?
+// when deal with primitive data types like string, number, boolean, null, undefined, symbol say it is pass by value   
+// when deal with non-primitive data types like object, array, function say it is pass by reference
+
+// when we clone the array or object and change the value of the cloned object or array it will change the value of the original object or array
 // Q:17 How execution context is created for the var and function?
 // Q:18 Props vs state
 // Q:19 What is HOC? Give example for the ecommerce website
--- objectives/index.js
+++ objectives/index.js
@@ -21,52 +21,314 @@
 // 2. Write code to find the missing range in the Array.
 // Example -> [1,3,5,8] will return [2,4,6,7]
 
-function missing(arr) {
-  let missings = [];
-  let max = arr[0];
-  console.log(max);
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  for (let i = 0; i < max; i++) {
-    if (arr.indexOf(i) == -1) {
-      missings.push(i);
-    }
-  }
-  console.log(max);
-  console.log(missings);
-}
+// function missing(arr) {
+//   let missings = [];
+//   let max = arr[0];
+//   console.log(max);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   for (let i = 0; i < max; i++) {
+//     if(arr.indexOf(i) === -1) {
+//     missings.push(i);
+//     }
+//   }
+//   console.log(max);
+//   console.log(missings);
+// }
 
-missing([1, 3, 5, 8]);
+// missing([1, 3, 5, 8]);
 
 // 3. Write the code to find indeces of the two elements of the array
+
 // that sum equal to the provided value
 // Example -> [1,2,3,4,5,6,7] sum=9
-// it will return the indeces of teh 4 and 5 (3,4)
+// it will return the indeces of the 4 and 5 (3,4)
+
+// const findIndeces = (arr, sum) => {
+//   let indeces = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === sum) {
+//         indeces.push({
+//           values: `${arr[i]} and ${arr[j]}`,
+//           indeces: `${i} and ${j}`,
+//         });
+//       }
+//     }
+//   }
+//   return indeces;
+// };
+
+// console.log(findIndeces([1, 2, 3, 4, 5, 6, 7], 9));
 
 // 4. Write the code for the Palindrome
 // Example -> 'madam' will return true
 
+// Algo
+// 1. make new str variable initially empty
+// 2. loop through the string from the end to start
+// 3. and add the char in new str variable
+// 4. compare the new str variable with the original str variable
+
+// const palindrome = (str)=>{
+//   var newStr = ''
+//   for(let i=str.length-1; i>=0; i--){
+//     newStr = newStr+str[i]
+//   }
+//   console.log('newStr', newStr)
+//   if(newStr === str) return true
+//   else return false
+// }
+// console.log(palindrome('madam'))
+
+// // 1. format the str (remove all white spaces)
+// // 2. convert str to arr and reverse it and convert to str
+// const palindrom = (str) =>{
+//   function format (str){
+//     return str.replace(/\s+/g,"")
+//   }
+//   return format(str).split('').reverse().join('') === format(str)
+// }
+// console.log(palindrom('a  lila '))
+
 // 5. Write the code for the fibonacci series
 // Example -> 1,1,2,3,5,8,13,21,34,55
 
+// Algo
+// 1. take a num as an argum and generate the series till the num
+// 2. if num is 1 generate the series [1]
+// 3. if two generate [1,1]
+// 4. if the num is more than two than
+// 5. make an array of fabSeries = [1,1]
+// 6. start a loop starting from 2 till the length of the num
+// 7. push the number with sum of previous two num of the series
+// 8. like fabSeries.push([i-1]+[i-1])
+
+// const fabonacci = (num)=>{
+//   if(num === 1) return [1]
+//   if(num === 2) return [1,1]
+//   let fabSeries = [1,1]
+//   for(let i=2; i<num; i++){
+//     fabSeries.push(fabSeries[i-1]+fabSeries[i-2])
+//   }
+//   return fabSeries
+// }
+
+// console.log(fabonacci(15))
+// const fibonacci = (num) => {
+//   if (num === 1) return [1];
+//   if (num === 2) return [1, 1];
+//   let series = [1, 1];
+//   for (let i = 2; i < num; i++) {
+//     series.push(series[i - 1] + series[i - 2]);
+//   }
+//   return series;
+// };
+
+// console.log(fibonacci(5));
 // 6. Write the code reverse the string and the integer
 
+// Algo
+// 1. make new variable
+// 2. loop through the old variable and store the char into new variable
+// 3. compare the new and old var
+
+// const reverseVar = (variable) =>{
+//   let newVar = ''
+//   function format(variable){
+//     return variable.replace(/[^a-zA-Z0-9\s]/g,"")
+//   }
+//   for(let i=variable.length-1;i>=0; i--){
+//     newVar+=variable[i]
+//   }
+//   return format(newVar)
+// }
+
+// console.log(reverseVar('my na_=+=-=me is hurairah;;'))
 // 7. Write the code to find the most repeated character in the string
 
+// 1. first find the count of all char and store in the array of Object
+// 2. than loop through the array and find the max count letter
+
+// const findFrequency = (str) => {
+//   let frequency = { char: "", count: 0 };
+//   let high = { char: "", count: 0 };
+//   for (let char of str) {
+//     frequency[char] = (frequency[char] || 0) + 1;
+//     if (frequency[char] > high.count) {
+//       high.count = frequency[char];
+//       high.char = char;
+//     }
+//   }
+//   return high;
+// };
+
+// console.log(findFrequency("aaann"));
+
+// const frequencyCounter = (str) => {
+//   // trim and convert to lowercase
+//   let newStr = str.replace(/\s/g, "").toLowerCase();
+//   let frequency = { char: "", count: 0 };
+//   let highFrequency = { char: "", count: 0 };
+//   for (let char of newStr) {
+//     frequency[char] = (frequency[char] || 0) + 1;
+//     if (frequency[char] > highFrequency.count) {
+//       highFrequency.count = frequency[char];
+//       highFrequency.char = char;
+//     }
+//   }
+//   return highFrequency;
+// };
+
+// console.log(frequencyCounter("My name is Abu Huraira"));
+
+// // Write the code to check the number is prime or not
+// const isPrime = (num) => {
+//   if (num < 2) return false;
+//   if (num === 2 || num === 3) return true;
+//   if (num > 3) {
+//     for (let i = 2; i < num; i++) {
+//       if (num % i === 0) {
+//         return false;
+//       } else {
+//         return true;
+//       }
+//     }
+//   }
+// };
+
+// console.log(isPrime(15));
+
 // 8. Write the code to chunk the array into the specified size
-// Example -> [1,2,3,4,5,6,7,8,9] size=3
-// it will return [[1,2,3],[4,5,6],[7,8,9]]
+// Example -> [1,2,3,4,5,6,7,8,9] size = 3
+// it will return [ [1,2,3]  , [4,5,6] , [7,8,9] ]
+
+// Algo
+// 1. Take arr and user provided size var
+// 2. take two parameters 'start' and 'end'
+// 3. end = start + userVar
+// 4. start = arraySize - start
+// 5. make base condition start < arraySize
+// 6. make two arrays before loop
+// 7. one is parent and second will be updated
+// 8. push the second array inside the paent array and empty
+// 9. and update start and end vars
+
+// const chunkArray = (arr, size) => {
+//   let parentArr = [];
+//   let newArr = [];
+//   let start = 0;
+//   let end = size;
+//   while (start < arr.length) {
+//     newArr = arr.slice(start, end);
+//     parentArr.push(newArr);
+//     newArr = [];
+//     start = end;
+//     end = start + size;
+//   }
+//   return parentArr;
+// };
+
+// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9,10], 3));
+
+// function chunks(arr, size) {
+//   let parentArr = [];
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     newArr.push(arr[i]);
+//     console.log(newArr)
+//     if (newArr.length === size) {
+//       parentArr.push(newArr);
+//       newArr = [];
+//     }
+//   }
+//   if(newArr.length< 3){
+//     parentArr.push(newArr);
+//   }
+//   return parentArr;
+// }
+// console.log(chunks([1, 2, 3, 4, 5, 6, 7, 8, 9,10],3));
 
 // 8. Write the code to find the longest word in the string
+
+// const res = { length: 0, word: "" };
+// const longestWord = (str) => {
+//   const word = () => {
+//     return str.split(" ").filter((word) => {
+//       if (word.length > res.length) {
+//         res.length = word.length;
+//         res.word = word;
+//       }
+//     });
+//   }
+//   word();
+//   return res
+// };
+// console.log(longestWord("my name is abu huraira"));
 
 // 9. Write the code for the Title Case. Every alphabet of the word will be capital
 // Example -> 'ali raza' will return 'Ali Raza'
 
+// Approach 1 -> convert str to array and change the first index of every word to uppercase
+// Approach 2 -> find the space and change the next index to uppercase
+// Approach 3 -> use the map function to change the first index of every word to uppercase
+
+// Approach 1 -> convert str to array and change the first index of every word to uppercase
+
+// const titleCase = (str) => {
+//   let words = str.split(" ");
+//     for(let i=0; i<words.length; i++) {
+//         words[i] = words[i][0].toUpperCase() + words[i].slice(1)
+//     }
+//     return words.join(" ");
+// };
+
+// console.log(titleCase("ali raza"));
+
+// Approach 2 -> find the space and change the next index to uppercase
+
+// const titleCase = (str) => {
+//     let newStr = "";
+//   for (let i=0; i<str.length; i++) {
+//    if(i === 0 || str[i-1] === ' '){
+//          newStr += str[i].toUpperCase();
+//    }
+//     else {
+//          newStr += str[i];
+//     }
+//   }
+//   return newStr;
+// };
+
+// console.log(titleCase("ali raza"));
+
+// // Approach 3 -> use the map function to change the first index of every word to uppercase
+// const titleCase = (str) => {
+//   return str
+//     .split(" ")
+//     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+//     .join(" ");
+// };
+
+// console.log(titleCase("my deco tech"));
+
+// const titleCase = (str = "abu huraira") => {
+//   for (let i = 0; i < str.length; i++) {
+//     if (i === 0 || str[i - 1] === " ") {
+//       str = str.slice(0,i) + str[i].toUpperCase() + str.slice(i + 1);
+//     }
+//   }
+//   return str;
+// };
+
+// console.log(titleCase());
+
 // 10. Write a code to discover the anagrams in the array of the strings
 // Example -> ['listen','silent','hello','world'] will return ['listen','silent']
+
 
 // 11. Write the code to count the vowels in the string
 // Ecampe -> 'hello world' will return 3
